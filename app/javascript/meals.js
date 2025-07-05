662b6db1dac335657580e7cdbf818eb23e4c18ec
Tom Van Manen <tom.on.github@gmail.com>
2025-07-04 20:08:54 -0700

    Toggle meal details by clicking meal titles

---
+++ app/javascript/meals.js
@@ -0,0 +1,21 @@
+const meal_titles = document.querySelectorAll(".meal-title");
+
+meal_titles.forEach(function(title) {
+  let meal_details = title.nextElementSibling;
+
+  if (!(meal_details.classList.contains("meal-details"))) {
+    console.log(
+      "My `meals.js` script may be broken because `.meal-details` \
+      is no longer the direct sibling neighbor of `.meal-title`."
+    );
+  }
+
+  title.addEventListener('click', e => {
+    if (meal_details.style.display !== 'initial') {
+          meal_details.style.display = 'initial';
+    }
+    else {
+        meal_details.style.display = 'none';
+    }
+  });
+});
